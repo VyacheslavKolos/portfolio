cde59432fcc93861c69be9bf43c68a671d163fc9
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-07-11 14:53:31 +0300

    portfolio-changes

--- src/pages/PortfolioPage/Portfolio.js
+++ src/pages/PortfolioPage/Portfolio.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import {Box} from '@mui/material';
 
 import css from './Portfolio.module.css';
 import movieApp from '../../images/movieApp.png';
 import netShop from '../../images/net-shop.png';
+import events from '../../images/events.png'
 
 const Portfolio = () => {
     return (
         <div className={css.portfolio}>
-
-            <div className={css.movieApp}>
+            <div className={css.eventsManager}>
 
                 <span className={css.PortfolioBigText}><p>Portfolio</p></span>
 
-                <h3>React movie application: work with Redux, REST API</h3>
+                <h2>Event manager with timezones</h2>
+                <img src={events} alt="events" />
+            </div>
+
+            <div className={css.delimiter}></div>
+
+            <div className={css.movieApp}>
+
+
+                <h2>React movie application: work with Redux, REST API</h2>
                 <img src={movieApp} alt="movie app"/>
             </div>
 
+            <div className={css.delimiter}></div>
+
             <div className={css.netShop}>
-                <h3> Internet-shop using HTML, CSS, adaptive design</h3>
+                <h2> Internet-shop using HTML, CSS, adaptive design</h2>
                 <img src={netShop} alt="net shop"/>
             </div>
+
+
+
         </div>
     );
 };
